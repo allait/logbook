4e94e0a01cc7acfc9f079442704c3d225b92cadb
Alexey Bezhan <al@fueledbylemons.com>
2016-08-29 19:37:11 +0100

    Display PRs for multiple repos in a single list
    Adds requests to download PRs and deployments for more than one
    repository and display a single flat list of all pull requests.

--- src/logbook/core.cljs
+++ src/logbook/core.cljs
@@ -14,22 +14,34 @@
 (defn log [message & args]
   (apply js/console.log (cons message (map pr-str args))))
 
-
-(defonce repos (r/atom {}))
+(defonce github-data (r/atom {}))
 
 (def github-api "https://api.github.com")
+(def repositories ["allait/test-deployments" "alphagov/digitalmarketplace-api"])
 
 (defn get-pulls [repo]
-  (log "Reloading")
+  (log (format "Requesting %s pull requests" repo))
   (http/get (format "%s/repos/%s/pulls" github-api repo)
             {:with-credentials? false
              :query-params {"state" "all"
                             "per_page" 100}}))
 
-(defonce init (do
-              (go (let [response (<! (get-pulls "allait/test-deployments"))]
-                    (swap! repos assoc-in
-                           ["allait/test-deployments" :pulls] (:body response))))))
+(defn get-deployments [repo]
+  (log (format "Requesting %s deployments" repo))
+  (http/get (format "%s/repos/%s/deployments" github-api repo)
+            {:with-credentials? false
+             :query-params {"per_page" 100}}))
+
+(defn list-pulls [data]
+  (sort #(compare (:title %1) (:title %2)) (apply concat (map :pulls (vals data)))))
+
+(defonce init
+  (go (doseq [repo-name repositories]
+    ;; FIXME this is sending requests one at a time
+    (let [pulls (<! (get-pulls repo-name))
+          deployments (<! (get-deployments repo-name))]
+      (swap! github-data assoc-in [repo-name :pulls] (:body pulls))
+      (swap! github-data assoc-in [repo-name :deployments] (:body deployments))))))
 
 (defn release-stats [{:keys [total env]}]
   (let [props-for (fn [name]
@@ -42,15 +54,19 @@
       [:li [:a (props-for :production) "Production"]]]]))
 
 (defn release-item []
-  (fn [{:keys [title]}]
-    [:li {:class "pull"}
+  (fn [{:keys [title url repo user state merged_at]}]
+    [:li {:class "pr"}
       [:div.view
-      [:span title]]]))
+       [:div.state state]
+       [:div.merged_at merged_at]
+       [:img.avatar {:src (:avatar_url user) :width 20 :height 20}]
+       [:a {:href url} title]]]))
 
 (defn release-app [props]
   (let [env (r/atom :preview)]
     (fn []
-      (let [items (:pulls (@repos "allait/test-deployments"))]
+      (let [items (list-pulls @github-data)]
+        (log "Items" items)
         [:div
          [:section#releases
           [:header#header
