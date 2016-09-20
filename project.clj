00fc69b034ff123f259e21b86182166ec575b484
Alexey Bezhan <al@fueledbylemons.com>
2016-09-20 21:44:41 +0100

    Add test build profile and a test runner

--- project.clj
+++ project.clj
@@ -7,9 +7,14 @@
                  [cljs-http "0.1.41"]]
 
   :plugins [[lein-figwheel "0.5.6"]
-            [lein-cljsbuild "1.1.3"]]
+            [lein-cljsbuild "1.1.3"]
+            [lein-doo "0.1.7"]]
 
   :source-paths ["src"]
+  :hooks [leiningen.cljsbuild]
+
+  :doo {:alias {:default [:phantom]}
+        :build "test"}
 
   :cljsbuild {:builds
               [{:id "dev"
@@ -20,10 +25,16 @@
                            :output-to "resources/public/js/main.js"
                            :output-dir "resources/public/js"
                            :source-map-timestamp true}}
+               {:id "test"
+                :source-paths ["src" "test"]
+                :compiler {:output-to "resources/test/js/main.js"
+                           :output-dir "resources/test/js"
+                           :main logbook.test.runner
+                           :optimizations :none}}
                {:id "min"
                 :source-paths ["src"]
                 :compiler {:output-to "resources/public/js/main.js"
                            :main logbook.core
                            :optimizations :advanced
                            :pretty-print false}}]}
-  :figwheel {})
+  :figwheel {:nrepl-port 7888})
